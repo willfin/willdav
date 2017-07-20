69d86cc3575fee867442e1bc50d89a34a8c0c950
willfindavid <willfin.david@episource.com>
2017-07-20 17:22:08 +0530

    Testing Now WillDav !!

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page !!!";
 
             return View();
         }
