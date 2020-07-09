50602470801c562336ad703223d24354a1d66c2e
willfin <mailwillfin@gmail.com>
2020-07-09 09:55:18 +0530

    Unit Test project included.

---
+++ iLessor_UnitTestProject/iLessor_UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("iLessor_UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("iLessor_UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1c221b3-e69d-4100-aadd-d328c3ff8f62")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ iLessor_UnitTestProject/iLessor_UnitTestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iLessor_UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReturnActual()
+        {
+            int firstValue = 20;
+            int secondValue = 2;
+            int sum = firstValue/secondValue;
+
+            Assert.AreEqual(10, sum);
+        }
+
+        [TestMethod]
+        public void GetName()
+        {
+            string name = "Mark";
+            Assert.AreEqual("Mark", name);
+        }
+    }
+}
